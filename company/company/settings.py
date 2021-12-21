3ee8d5a133b81cca6e2ead6d2c1a5dbbc4f6ec6c
rofar91 <rofar91@gmail.com>
2021-12-22 00:51:26 +0300

    added DRF authentication and permission settings

--- company/company/settings.py
+++ company/company/settings.py
@@ -30,6 +30,21 @@ ALLOWED_HOSTS = ['0.0.0.0']
 AUTH_USER_MODEL = 'employees.Employee'
 
 
+# DRF setting
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 10
+}
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,6 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
 
     'employees.apps.EmployeesConfig',
 ]
