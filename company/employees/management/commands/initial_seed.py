96ef95d309dc656e9bee706806edc39f7b4580e7
rofar91 <rofar91@gmail.com>
2021-12-22 23:34:16 +0300

    created command for seed DB, added fixtures

---
+++ company/employees/management/commands/initial_seed.py
@@ -0,0 +1,30 @@
+import random
+
+from django.core import management
+from django.core.management.base import BaseCommand
+from django_seed import Seed
+from rest_framework.authtoken.models import Token
+
+from employees.models import SalaryHistory, Employee
+
+
+class Command(BaseCommand):
+    help = 'Initial seed DB'
+
+    def handle(self, *args, **options):
+        # seed ranks and employees from fixtures
+        management.call_command('loaddata', 'ranks', verbosity=0)
+        management.call_command('loaddata', 'employees', verbosity=0)
+
+        # seed salary history used django-seed
+        seeder = Seed.seeder()
+        seeder.add_entity(SalaryHistory, 20, {
+            'employee': lambda x: Employee.objects.get(id=random.randint(1, 5)),
+            'amount_of_payment': lambda x: random.randint(1, 1234567890)/100,
+        })
+        seeder.execute()
+
+        # create token
+        Token.objects.create(user_id=1)
+
+        self.stdout.write(self.style.SUCCESS('Successfully seed DB'))
