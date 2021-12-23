e8dc438f7d370962abe40974ed8d2c0c58a2c740
rofar91 <rofar91@gmail.com>
2021-12-23 15:25:18 +0300

    refactored views, added 'is_staff' logic

--- company/employees/views.py
+++ company/employees/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
 
 from employees.models import Employee
 from employees.serializers import EmployeeSerializer
@@ -6,18 +6,34 @@ from employees.serializers import EmployeeSerializer
 
 class Employees(generics.ListAPIView):
     """
-    API endpoint that allows staff users viewed employees.
+    API endpoint that users can viewed information about himself.
     """
-    queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+
+        # get all employees for staff users
+        if user.is_staff:
+            return Employee.objects.all()
+
+        # or get information about himself
+        return Employee.objects.filter(id=user.id)
 
 
 class Hierarchy(generics.ListAPIView):
     """
-    API endpoint that allows staff users viewed employees by hierarchy level.
+    API endpoint that users can viewed employees by hierarchy level.
     """
     serializer_class = EmployeeSerializer
 
     def get_queryset(self):
         level = self.kwargs['level']
-        return Employee.objects.filter(hierarchy_level=level)
+        user = self.request.user
+
+        # get all employees for staff users
+        if user.is_staff:
+            return Employee.objects.filter(hierarchy_level=level)
+
+        # or get information about himself
+        return Employee.objects.filter(hierarchy_level=level, id=user.id)
